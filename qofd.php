e7c8b75972dd2d544fd6390def8ea67f744ac061
Bryce Campbell <tonyhawk2100@gmail.com>
2025-01-31 13:06:30 -0800

    add some more comments to make things clear.

--- qofd.php
+++ qofd.php
@@ -1,12 +1,16 @@
 <?php
 // import functions file, to load some necessary elements, including helper functions.
 include("functions.php");
+
+// load necessary information from files.
 $json = file_get_contents("questions.json") ?? "" ;
 $history_json = file_get_contents("history.json") ?? "";
 
+// attempt to load parse questions file.
 $questions = json_decode($json, true);
 $history;
 
+// attempt to parse history, if there is data to be found.
 if (strlen($history_json) == 0) {
     $history = [];
 } else {
@@ -15,11 +19,20 @@ if (strlen($history_json) == 0) {
 
 $today = date("Y-m-d");
 
+// create object to hold today's question.
 $question_of_the_day = new Question();
 $question_of_the_day->date = $today;
 
+// retrieve random index, in the event a question is not found.
 $selected_index = array_rand($questions, 1);
 
+/* 
+try to figure out if there is already a question for the day and
+assign it as question of the day if so.
+
+Otherwise, randomly pick a question and write it to the log 
+as the day's question.
+*/
 if (is_null(question_for_date($today, $history))) {
     $question_of_the_day->question = $questions[$selected_index];
     $history[] = $question_of_the_day;
